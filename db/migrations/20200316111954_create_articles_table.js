a2add3343226aae3bae765f579939c9bc56aecaa
James Cowell <jamescowell@Jamess-MacBook-Pro.local>
2020-03-16 11:52:01 +0000

    created migration file for comments

--- db/migrations/20200316111954_create_articles_table.js
+++ db/migrations/20200316111954_create_articles_table.js
@@ -3,8 +3,8 @@ exports.up = function(knex) {
   return knex.schema.createTable('articles', articlesTable => {
     articlesTable.increments('article_id').primary();
     articlesTable.string('title').notNullable();
-    articlesTable.string('body').notNullable();
-    articlesTable.int('votes').defaultTo(0);
+    articlesTable.text('body').notNullable();
+    articlesTable.integer('votes').defaultTo(0);
     articlesTable.string('topic').references('topics.slug');
     articlesTable.string('author').references('users.username');
     articlesTable
